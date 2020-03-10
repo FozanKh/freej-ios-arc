8fbebbbe856c30e6edd98baf09a730c3ffa8ad2d
Abdulelah Hajjar <abdulelahhajjar@gmail.com>
2020-03-10 18:36:49 +0300

    Moved project files to main directory

---
+++ Freej/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Freej
+//
+//  Created by Abdulelah Hajjar on 10/03/2020.
+//  Copyright © 2020 Squadra. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
