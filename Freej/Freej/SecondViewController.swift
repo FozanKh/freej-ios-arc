0e0cdb0949f584e2b5869320546791f5ac29fe31
Abdulelah Hajjar <abdulelahhajjar@gmail.com>
2020-03-10 18:24:46 +0300

    Upload initial Xcode project files

---
+++ Freej/Freej/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Freej
+//
+//  Created by Abdulelah Hajjar on 10/03/2020.
+//  Copyright © 2020 Squadra. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
