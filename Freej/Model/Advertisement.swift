b88e8690cdd8876173b09f591f476f08e348c57b
Abdulelah Hajjar <abdulelahhajjar@gmail.com>
2020-03-11 13:49:39 +0300

    Created MVC groups Created MaintenanceRequest, Announcement, Activity, Advertisement classes in Model group

---
+++ Freej/Model/Advertisement.swift
@@ -0,0 +1,13 @@
+//
+//  Advertisement.swift
+//  Freej
+//
+//  Created by Abdulelah Hajjar on 10/03/2020.
+//  Copyright © 2020 Squadra. All rights reserved.
+//
+
+import Foundation
+
+class Advertisement {
+    
+}
---
+++ Freej/Model/Announcement.swift
@@ -0,0 +1,13 @@
+//
+//  Announcement.swift
+//  Freej
+//
+//  Created by Abdulelah Hajjar on 10/03/2020.
+//  Copyright © 2020 Squadra. All rights reserved.
+//
+
+import Foundation
+
+class Announcement {
+    
+}
