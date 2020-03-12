2f6aadce75c3341d88902e802e6e3d30b07ae915
Abdulelah Hajjar <abdulelahhajjar@gmail.com>
2020-03-13 01:43:00 +0300

    Added welcomeView to the main.storyboard Added 4 text fields for preliminary implementation for the signup feature Added the picker view

---
+++ Freej/View/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  Freej
+//
+//  Created by Abdulelah Hajjar on 13/03/2020.
+//  Copyright © 2020 Squadra. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeView: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
