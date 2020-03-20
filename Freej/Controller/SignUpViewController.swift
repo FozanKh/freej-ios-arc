4e927cdc31dd46f5937a643a639ba55178e18e84
Abdulelah Hajjar <abdulelahhajjar@gmail.com>
2020-03-20 16:04:23 +0300

    Add SignUpViewController class Add ValidateCodeViewController class

---
+++ Freej/Controller/SignUpViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SignUpViewController.swift
+//  Freej
+//
+//  Created by Abdulelah Hajjar on 20/03/2020.
+//  Copyright © 2020 Squadra. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+}
