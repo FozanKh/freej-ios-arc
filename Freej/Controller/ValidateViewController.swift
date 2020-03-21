654e0497bb0ac6cad17717bcaefd7dbb3adcd2d3
Abdulelah Hajjar <abdulelahhajjar@gmail.com>
2020-03-22 00:51:26 +0300

    Implement showErrorSendingOTP alert whenever there is a problem connecting to the email service

--- Freej/Controller/ValidateViewController.swift
+++ Freej/Controller/ValidateViewController.swift
@@ -11,13 +11,13 @@ import MessageUI
 import Alamofire
 
 class ValidateViewController: UIViewController {
-    @IBOutlet weak var kfupmIDLabel: UILabel!
     var kfupmID: String!
 	var loginStatus: Bool!
     var correctPIN: String?
 	var otp: String!
 	var otpGenerationTime: Date!
 	
+	@IBOutlet weak var kfupmIDLabel: UILabel!
     @IBOutlet weak var validationCodeTF: UITextField!
     
     override func loadView() {
@@ -26,12 +26,30 @@ class ValidateViewController: UIViewController {
 		
 		generateOTP {
 			//After the successful generation of OTP, the code gets sent to the provided email
-			NetworkManager.sendOTP(toEmail: kfupmID + "@kfupm.edu.sa", otp: otp)
+			let status = NetworkManager.sendOTP(toEmail: kfupmID + "@kfupm.edu.sa", otp: otp)
+			if(status == false) {
+				showErrorSendingOTP()
+			}
 		}
     }
-    
-
 	
+	func showErrorSendingOTP() {
+		let alert = UIAlertController(title: "Error", message: "The application encountered an error while sending the OTP.", preferredStyle: .alert)
+		
+		alert.addAction(UIAlertAction(title: "Try Again", style: .default, handler: { (UIAlertAction) in
+			self.generateOTP {
+				let status = NetworkManager.sendOTP(toEmail: self.kfupmID + "@kfupm.edu.sa", otp: self.otp)
+				if(!status) {
+					self.showErrorSendingOTP()
+				}
+			}
+		}))
+		alert.addAction(UIAlertAction(title: "Cancel Login", style: .default, handler: { (UIAlertAction) in
+			(self.parent as! EnterFreejNavController).dismiss(loginStatus: false)
+		}))
+		self.present(alert, animated: true)
+	}
+    
 	func generateOTP(completion: () -> ()) {
 		otp = "\(Int.random(in: 1000...9999))"
 		otpGenerationTime = Date()
