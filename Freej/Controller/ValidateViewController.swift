dc99ccdc00ae84b0cb91f071298944d4b328835a
Abdulelah Hajjar <abdulelahhajjar@gmail.com>
2020-03-22 01:05:54 +0300

    Implement correct top checking (if user entered wrong otp)

--- Freej/Controller/ValidateViewController.swift
+++ Freej/Controller/ValidateViewController.swift
@@ -14,49 +14,43 @@ class ValidateViewController: UIViewController {
 	
     var kfupmID: String!
 	var loginStatus: Bool!
-	var otp: String!
+	var correctOtp: String!
 	var otpGenerationTime: Date!
+	var parentVC: EnterFreejNavController!
 	
     @IBOutlet weak var validationCodeTF: UITextField!
     
     override func loadView() {
-        super.loadView()		
-		generateOTP {
-			//After the successful generation of OTP, the code gets sent to the provided email
-			let status = NetworkManager.sendOTP(toEmail: kfupmID + "@kfupm.edu.sa", otp: otp)
-			if(status == false) {
-				showErrorSendingOTP()
-			}
-		}
+        super.loadView()
+		generateOTP()
     }
 	
 	@IBAction func loginButton(_ sender: Any) {
 		let userEnteredOTP = validationCodeTF.text ?? "0"
-		if(userEnteredOTP == otp) {
+		if(userEnteredOTP == correctOtp) {
 			(parent as! EnterFreejNavController).dismiss(loginStatus: true)
+		}
+		else {
+			showAlert(message: "The entered OTP did not match our records.")
 		}
 	}
 	
-	func showErrorSendingOTP() {
-		let alert = UIAlertController(title: "Error", message: "The application encountered an error while sending the OTP.", preferredStyle: .alert)
+	func showAlert(message: String) {
+		let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
 		
-		alert.addAction(UIAlertAction(title: "Try Again", style: .default, handler: { (UIAlertAction) in
-			self.generateOTP {
-				let status = NetworkManager.sendOTP(toEmail: self.kfupmID + "@kfupm.edu.sa", otp: self.otp)
-				if(!status) {
-					self.showErrorSendingOTP()
-				}
-			}
-		}))
+		alert.addAction(UIAlertAction(title: "Try Again", style: .default, handler: { (UIAlertAction) in self.generateOTP()}))
 		alert.addAction(UIAlertAction(title: "Cancel Login", style: .default, handler: { (UIAlertAction) in
 			(self.parent as! EnterFreejNavController).dismiss(loginStatus: false)
 		}))
 		self.present(alert, animated: true)
 	}
     
-	func generateOTP(completion: () -> ()) {
-		otp = "\(Int.random(in: 1000...9999))"
+	func generateOTP() {
+		correctOtp = "\(Int.random(in: 1000...9999))"
 		otpGenerationTime = Date()
-		completion()
+		let hasSent = NetworkManager.sendOTP(toEmail: kfupmID + "@kfupm.edu.sa", otp: correctOtp)
+		if(!hasSent) {
+			showAlert(message: "The application encountered an error while sending the OTP.")
+		}
 	}
 }
