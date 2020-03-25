cd0c65105ef92fbf8e7e2e3983e4cb7503b4b828
Abdulelah Hajjar <abdulelahhajjar@gmail.com>
2020-03-26 01:57:38 +0300

    Add comments in EnterFreejNavController

--- Freej/Controller/BeforeMainScreen/EnterFreejNavController.swift
+++ Freej/Controller/BeforeMainScreen/EnterFreejNavController.swift
@@ -9,18 +9,23 @@
 import UIKit
 
 class EnterFreejNavController: UINavigationController {
+	//This completion handler will be set by WelcomeViewController
 	var loginProcessCompletionHandler: ((Bool) -> ())?
 	
     override func loadView() {
         super.loadView()
+		//At this point, there must be a user in DataModel class (look at enterFreejBtn implementation)
 		DataModel.userIsSignedUp() ? showValidationScreen() : showSignUpScreen()
     }
 	
+	//This method is called by (ValidationViewController) which dismisses the VC and calls the completion handeler
+	//The completion handler is set by the WelcomeViewController
 	func finishedLoginProcess(loginStatus: Bool) {
 		self.dismiss(animated: true)
 		loginProcessCompletionHandler?(loginStatus)
 	}
     
+	//MARK:- Push Suitable View Controller Methods
     func showValidationScreen() {
         let vc = storyboard?.instantiateViewController(withIdentifier: "ValidateViewController") as! ValidateViewController
         pushViewController(vc, animated: false)
