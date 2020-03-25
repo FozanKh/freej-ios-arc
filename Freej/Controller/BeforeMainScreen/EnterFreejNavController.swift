e106975dbeec2d3a987150dc1d66d159e5db2785
Abdulelah Hajjar <abdulelahhajjar@gmail.com>
2020-03-25 22:08:56 +0300

    Delete isSignedUp variable Delete kfupmID variable Delete loginDelegate variable Delete LoginDelegate protocol Delete kfupmID assigning for the next VC in showSignUp + showValidation methods

--- Freej/Controller/BeforeMainScreen/EnterFreejNavController.swift
+++ Freej/Controller/BeforeMainScreen/EnterFreejNavController.swift
@@ -8,35 +8,19 @@
 
 import UIKit
 
-protocol LoginDelegate {
-    func didFinishLogInProcess(loginStatus: Bool)
-}
-
 class EnterFreejNavController: UINavigationController {
-    var isSignedUp: Bool!
-    var kfupmID: String! //this will be verified in the welcomeVC so it is safe to keep it (!)
-    
-    var loginDelegate: LoginDelegate?
-    
     override func loadView() {
         super.loadView()
-        isSignedUp ? showValidationScreen() : showSignUpScreen()
-    }
-    
-    func dismiss(loginStatus: Bool) {
-        dismiss(animated: true)
-        loginDelegate?.didFinishLogInProcess(loginStatus: loginStatus)
+		DataModel.currentUser == nil ? showSignUpScreen() : showValidationScreen()
     }
     
     func showValidationScreen() {
         let vc = storyboard?.instantiateViewController(withIdentifier: "ValidateViewController") as! ValidateViewController
-        vc.kfupmID = self.kfupmID
         pushViewController(vc, animated: false)
     }
     
     func showSignUpScreen() {
         let vc = storyboard?.instantiateViewController(withIdentifier: "SignUpViewController") as! SignUpViewController
-        vc.kfupmID = self.kfupmID
         pushViewController(vc, animated: false)
     }
 }
