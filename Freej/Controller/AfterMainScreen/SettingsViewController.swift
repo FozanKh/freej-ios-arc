3e33bacbb4c58283693148dfcec711b0ce386d23
Abdulelah Hajjar <abdulelahhajjar@gmail.com>
2020-03-26 21:33:48 +0300

    Set self as delegate for UITableView Set self as data source for UITableView Create method configureTableView Create right to left arabic writing Create footer for table Create 2 sections with variable number of rows for a section

--- Freej/Controller/AfterMainScreen/SettingsViewController.swift
+++ Freej/Controller/AfterMainScreen/SettingsViewController.swift
@@ -9,27 +9,47 @@
 import UIKit
 
 class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
-	
 	let tableView = UITableView()
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		view.addSubview(tableView)
+		tableView.delegate = self
+		tableView.dataSource = self
+		configureTableView()
+		view.backgroundColor = .systemGray5
+	}
+	
+	func configureTableView() {
+		title = "الإعدادات"
 		
+		view.addSubview(tableView)
+		navigationController?.navigationBar.subviews[1].semanticContentAttribute = .forceRightToLeft
 		tableView.translatesAutoresizingMaskIntoConstraints = false
 		tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
 		tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
 		tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
 		tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-		
-		title = "Settings"
-		tableView.delegate = self
-		tableView.dataSource = self
+		tableView.backgroundColor = .systemGray5
+		let test = UIView()
+		tableView.tableFooterView = test
+		tableView.sectionFooterHeight = 50
+	}
+	
+	func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+		return CGFloat(100.0)
 	}
 	
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		10
+		if(section == 0) {
+			return 3
+		}
+		else {
+			return 4
+		}
+	}
+	
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return 2
 	}
 	
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
