5c9282c3d554136cfa014ac8e8f7ff6e1b37bdbf
Abdulelah Hajjar <abdulelahhajjar@gmail.com>
2020-03-26 20:25:23 +0300

    Add navigation controller under tabor controller in storyboard Add view controller in navigationcontroller in storyboard Programmatically configure tableview in settingsvc

--- Freej/Controller/AfterMainScreen/SettingsViewController.swift
+++ Freej/Controller/AfterMainScreen/SettingsViewController.swift
@@ -8,6 +8,32 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
-    
+class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+	
+	let tableView = UITableView()
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.addSubview(tableView)
+		
+		tableView.translatesAutoresizingMaskIntoConstraints = false
+		tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+		tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+		tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+		tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+		
+		title = "Settings"
+		tableView.delegate = self
+		tableView.dataSource = self
+	}
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		10
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		return UITableViewCell()
+	}
+	
 }
