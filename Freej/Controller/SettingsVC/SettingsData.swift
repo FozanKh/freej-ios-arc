50d84f84de50f6e603aa0e487ca33c6f4ca9ed13
Abdulelah Hajjar <abdulelahhajjar@gmail.com>
2020-04-14 18:54:08 +0300

    Create SettingsData struct Create userSettings and appSettings arrays of SettingsCellData Add elements to appSettings and userSettings arrays Create SettignsCellData struct Create getImage() method in SettingsCellData struct Change SettiingsVC large title to Settings Change section height to 20 Implement didSelectRowAt delegate method Change numberOfRowsInSction implementation to follow SettingsData struct Change numberOfSections implementation to follow SettingsData Change cellForRowAt implementation to follow SettingsData

---
+++ Freej/Controller/SettingsVC/SettingsData.swift
@@ -0,0 +1,33 @@
+//
+//  SettingsData.swift
+//  Freej
+//
+//  Created by Abdulelah Hajjar on 14/04/2020.
+//  Copyright © 2020 Squadra. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct SettingsData {
+	static let userSettings = [	SettingsCellData(cellTitle: "Profile", cellIcon: "person", cellSegueID: "toEditProfile"),
+								SettingsCellData(cellTitle: "Recent Activity", cellIcon: "arrow.clockwise.circle", cellSegueID: "toAllActivities"),
+								SettingsCellData(cellTitle: "Personalization", cellIcon: "paintbrush", cellSegueID: "toPersonalization")]
+								
+	static let appSettings = [	SettingsCellData(cellTitle: "Report Problem", cellIcon: "exclamationmark.triangle", cellSegueID: "toReportProblem"),
+								SettingsCellData(cellTitle: "Contact Us", cellIcon: "paperplane", cellSegueID: "toContactUs"),
+								SettingsCellData(cellTitle: "Privacy Policy", cellIcon: "doc.plaintext", cellSegueID: "toPrivacypolicy"),
+								SettingsCellData(cellTitle: "Credits", cellIcon: "doc.plaintext", cellSegueID: "toCredits")]
+	
+	static let sections = [userSettings, appSettings]
+}
+
+struct SettingsCellData {
+	let cellTitle:	String
+	let cellIcon:	String
+	let cellSegueID:String
+	
+	func getImage() -> UIImage {
+		return UIImage(systemName: cellIcon) ?? UIImage(named: cellIcon) ?? UIImage.checkmark
+	}
+}
--- Freej/Controller/SettingsVC/SettingsVC.swift
+++ Freej/Controller/SettingsVC/SettingsVC.swift
@@ -20,40 +20,38 @@ class SettingsVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 	}
 	
 	func configureTableView() {
-		title = "الإعدادات"
+		title = "Settings"
 		
 		view.addSubview(tableView)
-		navigationController?.navigationBar.subviews[1].semanticContentAttribute = .forceRightToLeft
+//		navigationController?.navigationBar.subviews[1].semanticContentAttribute = .forceRightToLeft
 		tableView.translatesAutoresizingMaskIntoConstraints = false
 		tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
 		tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
 		tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
 		tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-		tableView.backgroundColor = .systemGray5
+		tableView.backgroundColor = .systemGroupedBackground
 		let test = UIView()
 		tableView.tableFooterView = test
-		tableView.sectionFooterHeight = 50
+		tableView.sectionFooterHeight = CGFloat(20.0)
 	}
 	
-	func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-		return CGFloat(100.0)
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		let segueID = SettingsData.sections[indexPath.section][indexPath.row].cellSegueID
+		performSegue(withIdentifier: segueID, sender: self)
 	}
 	
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		if(section == 0) {
-			return 3
-		}
-		else {
-			return 4
-		}
+		return SettingsData.sections[section].count
 	}
 	
 	func numberOfSections(in tableView: UITableView) -> Int {
-		return 2
+		return SettingsData.sections.count
 	}
 	
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		return UITableViewCell()
+		let cell = UITableViewCell()
+		cell.textLabel?.text = (SettingsData.sections[indexPath.section][indexPath.row]).cellTitle
+		cell.imageView?.image = (SettingsData.sections[indexPath.section][indexPath.row]).getImage()
+		return cell
 	}
-	
 }
