821d210ec53a5dd3e4ac9b0212c06d0b22f14a75
Abdulelah Hajjar <abdulelahhajjar@gmail.com>
2020-04-15 16:17:46 +0300

    Delete unused comments

--- Freej/Controller/PreLoginVC/SignUpVC.swift
+++ Freej/Controller/PreLoginVC/SignUpVC.swift
@@ -9,9 +9,8 @@
 /*
 	Upon clicking on sign up button, the user will not immediately added to the database.
 	The user must initially verify his identity by entering the correct code sent to their email.
-	Therefore, as seen in the implementation, upon clicking signup, the KFUPM ID is sent to the next VC.
-	The newUserHasValidated delegate method will be called by the ValidateViewController upon the successful entering of the OTP.
 */
+
 import UIKit
 
 class SignUpVC: UIViewController {
@@ -34,44 +33,4 @@ class SignUpVC: UIViewController {
 		DataModel.currentUser!.bno = bNoTF.text!
 		performSegue(withIdentifier: "toValidateCodeFromSignUp", sender: self)
     }
-	
-	//MARK:- Misc. Methods
-	func showAlert(message: String) {
-//		let parentVC = parent as! EnterFreejNC
-		let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
-//		alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: { (UIAlertAction) in parentVC.popViewController(animated: true)}))
-		self.present(alert, animated: true)
-	}
 }
-
-//This delegate method will get called by the ValidationController upon the SUCCESS of OTP code validation.
-//Since the user in DataModel.currentUser is not complete (i.e. only has KFUPM ID)
-//check enterFreejBtn implementation for further explanation in WelcomeViewController.
-//The user will be added to the database at this point (after corrct OTP)
-//The user will be re-downloaded from the Database (we need this to save his user id in the object)
-//And after completion, the handler will be called with either true or false to the ValidateViewControleler
-//ValidateViewController will then based on the returned result, dismiss the view and login the user if (true)
-/*func newUserHasValidated(completion: @escaping (Bool, String?) -> ()) {
-NetworkManager.signUpUser(kfupmIDTF.text!, fNameTF.text!, lNameTF.text!, bNoTF.text!) { (signUpStatus) in
-if(signUpStatus == true) {
-
-NetworkManager.getStudent(kfupmID: self.kfupmIDTF.text!) { (userInfo) in
-if(userInfo != nil) {
-completion(true, nil)
-//Save to persistent because the user is successfully logged-in
-
-//						DataModel.setSignedUpUser(userJSON: userInfo!, saveToPersistent: true)
-}
-else {
-//Clear from persistent, and local memory because login process was failed.
-DataModel.clearCurrentUser()
-completion(false, "Error: Could not login user.")
-}
-}
-
-}
-else {
-completion(false, "Error: Could not sign up user.")
-}
-}
-}*/
